f942d3dc5444ee3e66f4dbec79df6277e0742184
Dasha080403 <savinkina2003@mail.ru>
2026-03-04 13:20:56 +0300

    Initial Commit

---
+++ TrackerTests/TrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  TrackerTests.swift
+//  TrackerTests
+//
+//  Created by Дарья Савинкина on 04.03.2026.
+//
+
+import Testing
+@testable import Tracker
+
+struct TrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
